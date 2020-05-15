42d40d3ce743a39a320cd8d7fcdad02a4706c032
Aarushi Gupta <aarushigupta55555@gmail.com>
2020-05-15 22:52:53 +0530

    Display a message using the username obtained

--- tool.py
+++ tool.py
@@ -1,4 +1,3 @@
 email = input("Enter your email ID: ")
 split_list = email.split("@")
-print(f"Your username is: {split_list[0]}")
-print(f"Domain part of the email is: {split_list[1]}")
+print(f"Message: Thank you {split_list[0]} for using my Email Slicing tool!")
